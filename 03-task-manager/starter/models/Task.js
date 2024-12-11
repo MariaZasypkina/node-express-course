259b47c33bdfcb2614ff22ed5c6baf9c2dc0065d
MariaZasypkina <maria.zasypkina@gmail.com>
2024-12-10 22:55:52 -0700

    second commit week 5

---
+++ 03-task-manager/starter/models/Task.js
@@ -0,0 +1,14 @@
+
+
+const mongoose = require('mongoose')
+
+const TaskSchema = new mongoose.Schema({
+  name: {
+    type: String,
+  },
+  completed: {
+    type: Boolean,
+  },
+})
+
+module.exports = mongoose.model('Task', TaskSchema)
